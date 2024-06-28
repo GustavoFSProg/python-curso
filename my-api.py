b7724a141a79467f115968d745702ff895dc7050
GusFSProg <gustavo.prog40@gmail.com>
2024-06-28 20:14:55 -0300

    comitando

--- my-api.py
+++ my-api.py
@@ -7,12 +7,11 @@ st.set_page_config(layout="wide")
 def buscar_dados():
     request = requests.get("https://blog-gus-api.vercel.app/get-all-posts")
     todos = json.loads(request.content)
+    todos[0]
     todos
     # print(todos)
     # print(todos[1]['title'])
     # print(todos[1]['text'])
 
 # if __name__ == '__main__':
-datas =  buscar_dados()
-
-datas
+buscar_dados()
