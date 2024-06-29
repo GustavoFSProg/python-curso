ced2895a4974ac8c904e096c0e712af607091c4d
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2024-06-29 18:16:53 -0300

    my-site

--- Curso Python PowerDAta/Aula - 03/pedidos.py
+++ Curso Python PowerDAta/Aula - 03/pedidos.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import streamlit as st
+import numpy
 
 st.set_page_config(layout="wide")
+
+# st.write("Entrou aqui")
 
 # ped = pd.read_excel("vendas.xlsx")
 ped = pd.read_excel("gente.xlsx")
 
-dados = ped.head()
+# dados = ped.head()
 # dados = ped.tail()
 # dados = ped.preco
+
+dados = "OOOOOIIIII!"
 
 dados
 
